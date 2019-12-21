f136be431d919af9d7934406ef95bfe3a380a47c
Andrea Stefanini <andreastefanini98@gmail.com>
2019-12-21 09:00:37 +0100

    further more improvment done to the method of showing images

--- pappa.js
+++ pappa.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-    $("img:not(:nth-of-type(1))").hide();
+    console.log("Hello from the script");
+    let images = $("a:not(.nav-item)");
+
+    $("a:not(.nav-item):not(:nth-of-type(1))").hide();
+    let ButtonPrev = $("button#prev");
+    let ButtonNext = $("button#next");
+    let CurrentImg =1;
+    let NumofImg = (images.length);
+    console.log(NumofImg);
+    ButtonPrev.click(function(e){
+        e.preventDefault();
+        if(CurrentImg==1){
+            $("a:not(.nav-item):not(:nth-of-type("+ NumofImg+"))").fadeOut("slow");
+            $("a:not(.nav-item):nth-of-type("+ NumofImg+")").fadeIn("slow");
+            CurrentImg=NumofImg;
+        }else{
+            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeOut("slow");
+            CurrentImg--;
+            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeIn("slow");
+        }
+    });
+    ButtonNext.click(function(e){
+        e.preventDefault();
+        if(CurrentImg==NumofImg){
+            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeOut("slow");
+            CurrentImg=1;
+            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeIn("slow");
+        }else{
+            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeOut("slow");
+            CurrentImg++;
+            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeIn("slow");
+        }
+    });
 });
