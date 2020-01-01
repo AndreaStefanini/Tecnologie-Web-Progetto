3e850f952bcced0350168e3601cf3e4c1211dbe7
Andrea Stefanini <andreastefanini98@gmail.com>
2020-01-01 10:37:28 +0100

    added the new section Prossimi Eventi Disponibili, pretty happy for now with the result

--- pappa.js
+++ pappa.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     console.log("Hello from the script");
-    let images = $("a:not(.nav-item)");
+    let images = $("a.PP");
 
-    $("a:not(.nav-item):not(:nth-of-type(1))").hide();
+    $("a.PP:not(:nth-of-type(1))").hide();
+    $("span:not(:nth-of-type(1))").hide();
     let ButtonPrev = $("button#prev");
     let ButtonNext = $("button#next");
     let CurrentImg =1;
@@ -11,25 +12,33 @@ $(document).ready(function(){
     ButtonPrev.click(function(e){
         e.preventDefault();
         if(CurrentImg==1){
-            $("a:not(.nav-item):not(:nth-of-type("+ NumofImg+"))").fadeOut("slow");
-            $("a:not(.nav-item):nth-of-type("+ NumofImg+")").fadeIn("slow");
+            $("a.PP:not(:nth-of-type("+ NumofImg+"))").fadeOut("slow");
+            $("span:not(:nth-of-type("+ NumofImg+"))").hide();
+            $("a.PP:nth-of-type("+ NumofImg+")").fadeIn("slow");
+            $("span:not(:nth-of-type("+ NumofImg+"))").show();
             CurrentImg=NumofImg;
         }else{
             $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeOut("slow");
+            $("span:nth-of-type("+ CurrentImg+")").hide();
             CurrentImg--;
             $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeIn("slow");
+            $("span:nth-of-type("+ CurrentImg+")").show();
         }
     });
     ButtonNext.click(function(e){
         e.preventDefault();
         if(CurrentImg==NumofImg){
-            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeOut("slow");
+            $("a.PP:nth-of-type("+CurrentImg+")").fadeOut("slow");
+            $("span:nth-of-type("+ CurrentImg+")").hide();
             CurrentImg=1;
-            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeIn("slow");
+            $("a.PP:nth-of-type("+CurrentImg+")").fadeIn("slow");
+            $("span:nth-of-type("+ CurrentImg+")").show();
         }else{
-            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeOut("slow");
+            $("a.PP:nth-of-type("+CurrentImg+")").fadeOut("slow");
+            $("span:nth-of-type("+ CurrentImg+")").hide();
             CurrentImg++;
-            $("a:not(.nav-item):nth-of-type("+CurrentImg+")").fadeIn("slow");
+            $("a.PP:nth-of-type("+CurrentImg+")").fadeIn("slow");
+            $("span:nth-of-type("+ CurrentImg+")").show();
         }
     });
 });
