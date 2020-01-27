56460b24470861bf57688e010a0425fec1e5a3c3
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2020-01-27 18:12:54 +0100

    carousel responsive

--- pappa.js
+++ pappa.js
@@ -1,17 +1,2 @@
-const CarouselSlide = $("div.carousel-slide");
-const CarouselImages = $("div.carousel-slide a");
 
-//Buttons
-const PrevBtn= $("button#prev");
-const NextBtn= $("button#next");
-
-//Counter
-let counter =1;
-const size= CarouselImages[0].clientWidth;
-
-CarouselImages.style.transform = 'traslateX(' + (-size*counter) + 'px)';
-
-PrevBtn.addActionListener('click',()=>{
-
-});
 
