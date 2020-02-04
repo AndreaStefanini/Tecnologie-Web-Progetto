fe4879d519bd9023c3ae86214e7697f16ef30a65
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2020-02-04 15:52:23 +0100

    more functions added tpward the database

--- database.php
+++ database.php
@@ -10,11 +10,29 @@ class database {
     }
     public function add_user($nome, $cognome, $email, $userpassword, $data_nascita, $usertype){
        $insertquery=$this->connection->prepare("INSERT INTO users(Nome,Cognome,email,password,Data_Nascita,Tipo_User) VALUES(?,?,?,?,?,?)");
-       $insertquery->bind_param("s,s,s,s,d,s",$nome,$cognome,$email,$userpassword,$data_nascita,$usertype);
+       $insertquery->bind_param("ssssds",$nome,$cognome,$email,$userpassword,$data_nascita,$usertype);
        $insertquery->execute();
     }
     public function login($email, $password){
-        
+        $login=$this->connection->prepare("SELECT Nome, Cognome FROM Users WHERE email= ? and password= ?");
+        $login->bind_param("ss", $email, $password);
+        $login->execute();
+        $result = $login->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    public function add_article($title,$costo,$time,$date,$location,$image_path){
+        $article = $this->connection->prepare("INSERT INTO articles(Article_tile,Cost_Ticket,Time_Event,Date_Event,Location_Event,Image_Event) VALUES(?,?,?,?,?,?)");
+        $article->bind_param("sibbss",$title,$costo,$time,$date,$location,$image_path);
+        $article->execute();
+        $result = $article->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    public function get_article($idarticle){
+        $get=$this->connection->prepare("SELECT Article_Title,Costo_Ticket,Date_Event,Time_Event,Location_Event,Image_Path FROM articles WHERE articles.ID_Article=?");
+        $get->bind_param("i",$idarticle);
+        $get->execute();
+        $result=$get->get_result();
+        return $result->fetch_all();
     }
 }
 ?>
