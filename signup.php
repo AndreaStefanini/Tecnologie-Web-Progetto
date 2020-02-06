141802be631f808aaaccbebece110ffaf9d465fe
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2020-02-06 15:16:10 +0100

    further more improved the SignUp page and the transfer of data to the database

--- signup.php
+++ signup.php
@@ -3,6 +3,9 @@ require_once("database-entrance.php");
 if(isset($_GET["email"]) && isset($_GET["password"]) && isset($_GET["nome"]) && isset($_GET["datanascita"]) && isset($_GET["tipouser"])){
     if(ContainsNumbers($_GET["password"]) && ContainsUpperCase($_GET["password"]) && ContainsPunctuation($_GET["password"]) && strlen($_GET["password"])>=8){
         $db->add_user($_GET["nome"],$_GET["cognome"],$_GET["email"],$_GET["password"],$_GET["datanascita"],$_GET["tipouser"]);
+    }else{
+        echo "<script type='text/javascript'>alert('Your password is too weak for our standard, please create a more secure password, you will redirected to the sign page')</script>";
+        require("SignUp.html");
     }
 }
 
