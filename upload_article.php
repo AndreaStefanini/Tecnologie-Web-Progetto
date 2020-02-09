f80774eb51d8cc7ed4aba0d33b751fc26c7fd65d
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2020-02-09 09:50:22 +0100

    now it is possibile to upload image on the server, now i'll try to modify the photo's dimensions to make them equaly big

--- upload_article.php
+++ upload_article.php
@@ -1,4 +1,24 @@
 <?php
+require_once('database-entrance.php');
+
+    print_r($_FILES["EventFoto"]) ;
+    $filename = $_FILES["EventFoto"]["name"];
+    $filetype = $_FILES["EventFoto"]["type"];
+    $filepath = $_FILES["EventFoto"]["tmp_name"];
+    $fileerror = $_FILES["EventFoto"]["error"];
+    $filesize = $_FILES["EventFoto"]["size"];
+    
+
+    $allowedExt= array("jpeg","jpg", "png");
+    $tmpext =explode(".", $filename);
+    $fileExt = strtolower(end($tmpext));
+    if(in_array($fileExt, $allowedExt)){
+        $fileNewDestionation= 'resources/'.$_POST["ArticleTitle"].".".$fileExt;
+        move_uploaded_file($filepath, $fileNewDestionation);
+        echo " file uploaded successfully";
+    }else{
+        echo "<script> alert('We are sorry to inform you that the type of image you uploaded is not alllowed, please try again');</script>";
+    }
 
 
 
