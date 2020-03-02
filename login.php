9843319e4912daaa12b69812d78db3a555647724
Enrico Baroni <enrico.baroni4@studio.unibo.it>
2020-03-02 17:20:27 +0100

    bene

--- login.php
+++ login.php
@@ -1,12 +1,19 @@
 <?php
 require_once("database-entrance.php");
 
-if(isset($_GET["email"])&& isset($_GET["password"])){
-    $result=$db->login($_GET["email"],$_GET["password"]);
+if(isset($_POST["email"])&& isset($_POST["password"])){
+    $result=$db->login($_POST["email"],$_POST["password"]);
     if(count($result)==0){
        echo "Error";
     }else{
-        echo "Welcome: " . $result[0]["Nome"];
+        //echo "Welcome: " . $result[0]["Nome"];
+        session_start();
+        //$_SESSION["ID"]= $result["ID"];
+        $_SESSION["email"]= $result["email"];
+        
+        header("Location: ../BOPLEVE.html");
+        exit();
+
     }
 }
 
