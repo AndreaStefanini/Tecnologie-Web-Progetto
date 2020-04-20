690d89f11541d7484b67d5ec638480e83ba8b2a4
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2020-04-20 17:00:04 +0200

    breaked updating images in utilities functions

--- signup.php
+++ signup.php
@@ -1,74 +1,38 @@
 <?php
 require_once("database-entrance.php");
-function resizeImage($resourceType,$image_width,$image_height) {
-    $resizeWidth = 35;
-    $resizeHeight = 35;
-    $imageLayer = imagecreatetruecolor($resizeWidth,$resizeHeight);
-    imagecopyresampled($imageLayer,$resourceType,0,0,0,0,$resizeWidth,$resizeHeight, $image_width,$image_height);
-return $imageLayer;
-}
-if(isset($_POST["submit"])){
-    if(ContainsNumbers($_POST["password"]) && ContainsUpperCase($_POST["password"]) && ContainsPunctuation($_POST["password"]) && strlen($_POST["password"])>=8){
-        $NewFolderName = $_POST["nome"].$_POST["cognome"];
-        mkdir("resources/Users/".$NewFolderName, 7000);
-        mkdir("resources/Users/".$NewFolderName."/Articoli", 7000);
-        mkdir("resources/Users/".$NewFolderName."/Profilo", 7000);
-        if(is_array($_FILES)) {
-            $fileName = $_FILES['ProfileFoto']['tmp_name']; 
-            $sourceProperties = getimagesize($fileName);
-            $resizeFileName = $NewFolderName;
-            $uploadPath = "resources/Users/".$NewFolderName."/Profilo/";
-            $newpath = "resources/Users/".$NewFolderName."/Profilo/";
-            $fileExt = pathinfo($_FILES['ProfileFoto']['name'], PATHINFO_EXTENSION);
-            $uploadImageType = $sourceProperties[2];
-            $sourceImageWidth = $sourceProperties[0];
-            $sourceImageHeight = $sourceProperties[1];
-            switch ($uploadImageType) {
-                case IMAGETYPE_JPEG:
-                    $resourceType = imagecreatefromjpeg($fileName); 
-                    $imageLayer = resizeImage($resourceType,$sourceImageWidth,$sourceImageHeight);
-                    imagejpeg($imageLayer,$uploadPath.$NewFolderName.'.'. $fileExt);
-                    $resizeFileName=$newpath.$NewFolderName.'.'. $fileExt;
-                    break;
-                case IMAGETYPE_GIF:
-                    $resourceType = imagecreatefromgif($fileName); 
-                    $imageLayer = resizeImage($resourceType,$sourceImageWidth,$sourceImageHeight);
-                    imagegif($imageLayer,$uploadPath.$NewFolderName.'.'. $fileExt);
-                    $resizeFileName=$newpath.$NewFolderName.'.'. $fileExt;
-                    break;
-                case IMAGETYPE_PNG:
-                    $resourceType = imagecreatefrompng($fileName); 
-                    $imageLayer = resizeImage($resourceType,$sourceImageWidth,$sourceImageHeight);
-                    imagepng($imageLayer,$uploadPath.$NewFolderName.'.'. $fileExt);
-                    $resizeFileName=$newpath.$NewFolderName.'.'. $fileExt;
-                    break;
-                default:
-                    break;
-            } 
-            
-            $db->add_user($_POST["nome"],$_POST["cognome"],$_POST["email"],$_POST["password"],$_POST["datanascita"],$_POST["tipouser"],$resizeFileName);
-            header("Location: index.php");
-        }
-    }else{
+require_once("utilities/utility-functions.php");
+if (isset($_POST["submit"])) {
+    if (ContainsNumbers($_POST["password"]) && ContainsUpperCase($_POST["password"]) && ContainsPunctuation($_POST["password"]) && strlen($_POST["password"]) >= 8) {
+        $NewFolderName = $_POST["nome"] . $_POST["cognome"];
+        mkdir("resources/Users/" . $NewFolderName, 7000);
+        mkdir("resources/Users/" . $NewFolderName . "/Articoli", 7000);
+        mkdir("resources/Users/" . $NewFolderName . "/Profilo", 7000);
+        $subfolder = "/Profilo/";
+        $resizeFileName = saveImage($_FILES["ProfileFoto"], $NewFolderName,$subfolder, 20, 20);
+        $db->add_user($_POST["nome"], $_POST["cognome"], $_POST["email"], $_POST["password"], $_POST["datanascita"], $_POST["tipouser"], $resizeFileName);
+        header("Location: index.php");
+    } else {
         unset($_POST);
         echo "<script type='text/javascript'>alert('La Password inserita non è abbastanza forte per favore inserire una password che contega A-Z,a-z,0-9,,-');\n
         window.location.assign('signup.php');</script>";
     }
-}else{
-        
-        $page = "signup-form.php";
-        require("Bopleve.php");
-    }
+} else {
+
+    $page = "signup-form.php";
+    require("Bopleve.php");
+}
 
 
 
-function ContainsNumbers($String){
+function ContainsNumbers($String)
+{
     return preg_match('/\\d/', $String) > 0;
 }
-function ContainsUpperCase($String){
-    return preg_match('/[A-Z]/', $String) >0;
+function ContainsUpperCase($String)
+{
+    return preg_match('/[A-Z]/', $String) > 0;
 }
-function ContainsPunctuation($String){
-    return preg_match('/[,.-]/',$String) > 0;
+function ContainsPunctuation($String)
+{
+    return preg_match('/[,.-]/', $String) > 0;
 }
-?>
--- utilities/utility-functions.php
+++ utilities/utility-functions.php
@@ -1,1 +1,45 @@
-<?php ?>
+<?php
+session_start();
+function resizeImage($resourceType, $image_width, $image_height, $newwidth, $newheight)
+{
+    $resizeWidth = $newwidth;
+    $resizeHeight = $newheight;
+    $imageLayer = imagecreatetruecolor($resizeWidth, $resizeHeight);
+    imagecopyresampled($imageLayer, $resourceType, 0, 0, 0, 0, $resizeWidth, $resizeHeight, $image_width, $image_height);
+    return $imageLayer;
+}
+function saveImage($image, $fotoname,$subfolder, $newwidth, $newheight)
+{
+    $fileName = $image['tmp_name'];
+    $sourceProperties = getimagesize($fileName);
+    $resizeFileName = $fotoname;
+    $uploadPath = "resources/Users/" . $fotoname.$subfolder;
+    $newpath = "resources/Users/" . $fotoname.$subfolder;
+    $fileExt = pathinfo($image['name'], PATHINFO_EXTENSION);
+    $uploadImageType = $sourceProperties[2];
+    $sourceImageWidth = $sourceProperties[0];
+    $sourceImageHeight = $sourceProperties[1];
+    switch ($uploadImageType) {
+        case IMAGETYPE_JPEG:
+            $resourceType = imagecreatefromjpeg($fileName);
+            $imageLayer = resizeImage($resourceType, $sourceImageWidth, $sourceImageHeight,$newwidth, $newheight);
+            imagejpeg($imageLayer, $uploadPath . $fotoname . '.' . $fileExt);
+            $resizeFileName = $newpath . $fotoname . '.' . $fileExt;
+            break;
+        case IMAGETYPE_GIF:
+            $resourceType = imagecreatefromgif($fileName);
+            $imageLayer = resizeImage($resourceType, $sourceImageWidth, $sourceImageHeight,$newwidth, $newheight);
+            imagegif($imageLayer, $uploadPath . $fotoname . '.' . $fileExt);
+            $resizeFileName = $newpath . $fotoname . '.' . $fileExt;
+            break;
+        case IMAGETYPE_PNG:
+            $resourceType = imagecreatefrompng($fileName);
+            $imageLayer = resizeImage($resourceType, $sourceImageWidth, $sourceImageHeight,$newwidth, $newheight);
+            imagepng($imageLayer, $uploadPath . $fotoname . '.' . $fileExt);
+            $resizeFileName = $newpath . $fotoname . '.' . $fileExt;
+            break;
+        default:
+            break;
+    }
+    return $resizeFileName;
+}
