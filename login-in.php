64351db08c304b355d04b7d292691603d834da02
Andrea Stefanini <andrea.stefanini5@studio.unibo.it>
2020-05-05 17:08:58 +0200

    working on the modify article mechanism

--- login-in.php
+++ login-in.php
@@ -56,7 +56,7 @@ if ($db->is_admin($_SESSION["ID"])) {
         <div class="row logged">
             <img src="<?php echo $article["Image_Path"]; ?>" alt="" class="shared">
             <div style="margin-top:5%;"> <?php echo $article["Article_Title"] ?></div>
-            <button class="login icon"><img src="resources/matita.png" class="icons" alt="" onclick="window.location.replace('modify_article.php?ID=<?php echo $article['ID']; ?>');"></button>
+            <button class="login icon"><img src="resources/matita.png" class="icons" alt="" onclick="window.location.replace('modify_article.php?ID=<?php echo $article['ID_Articles']; ?>');"></button>
             <button class="login icon" id="delete_article"><img src="resources/bidone.png" class=" icons" onClick="window.location.href='delete-article?delete=<?php echo $article['Article_Title']; ?>'" alt=""></button>
 
         </div>
