568235f4b9fee020ce512ea574e6a4bf94f29e29
Andrea Stefanini <andreastefanini98@gmail.com>
2020-05-25 15:26:34 +0200

    improved sendemail, to work with only one event from cart

--- sendEmail.php
+++ sendEmail.php
@@ -17,10 +17,11 @@ if($_POST["numero_eventi"]>1){
 }else{
     $currentDate = date("Y-m-d");
     $db->add_purchase_to_acquisti($_SESSION["ID"],$_POST["id"],$_POST["n_tickets"],$currentDate);
-    if($_POST["n_tickets"]>1){
-        $body = "Salve, ".$_SESSION["nome"] ." ".$_SESSION["cognome"].". I ". $_POST["n_tickets"]." biglietti per l'evento: ".$_POST["event"]. " sono stati comprati con successo ";
+    $acquisto = $db->get_latest_purchases($_SESSION["ID"],$_POST["numero_eventi"]);
+    if($acquisto[0]["n_tickets"]>1){
+        $body = "Salve, ".$_SESSION["nome"] ." ".$_SESSION["cognome"].". I ". $acquisto[0]["n_tickets"]." biglietti per l'evento: ".$acquisto[0]["Article_Title"]. " sono stati comprati con successo ";
     }else{
-        $body = "Salve, ".$_SESSION["nome"] ." ".$_SESSION["cognome"].". Il biglietto per l'evento: ". $_POST["event"]." è stato comprato con successo ";
+        $body = "Salve, ".$_SESSION["nome"] ." ".$_SESSION["cognome"].". Il biglietto per l'evento: ". $acquisto[0]["Article_Title"]." è stato comprato con successo ";
     }
 }
 if (mail($to_email, $subject, $body)) {
